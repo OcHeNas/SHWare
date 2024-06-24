5cea98119da666f659e86cc6b9162c17f0f15885
OcHeNas <danila-erudit56546@mail.ru>
2024-06-25 00:08:40 +0300

    Настройка проверки прав пользователя

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -10,13 +10,13 @@ from app.exceptions import (
 )
 from app.users.dao import UserDAO
 from app.users.models import Users
-
+# Получение токена
 def get_token(request: Request):
        token = request.cookies.get("my_access_token")
        if not token:
           raise TokenAbsentException
        return token
-
+# Получение пользователя
 async def get_current_user(token: str = Depends(get_token)):
     try:
         payload = jwt.decode(
@@ -34,7 +34,7 @@ async def get_current_user(token: str = Depends(get_token)):
         raise UserIsNotPresentException
 
     return user
-
+# Проверка прав пользователя
 async def get_current_admin_user(current_user: Users = Depends(get_current_user)):
     if current_user.role_id != 1:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
